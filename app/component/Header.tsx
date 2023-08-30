d7ea8d631ebac41a45fe504a0f3b45b1cca942a4
Tao <a.koreta@outlook.fr>
2023-08-30 11:11:47 +0200

    add anim of hamburger menu

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -17,6 +17,7 @@ import ScreenSize from "@/app/utils/ScreenSizeUtils";
 const Header = () => {
     const dispatch = useDispatch();
     const isOpenMenu = useSelector((state:RootState) => state.menu.value);
+    const [animMenu,setAnimMenu] = useState(false);
     const [width,height] = ScreenSize();
 
 
@@ -44,8 +45,49 @@ const Header = () => {
 
 
             <div className={styles.menu}>
-                <div onClick={() => dispatch(toogle())}>
-                    <MenuSvg />
+                <div className={animMenu ? styles.active : ' '} onClick={() => {
+                    setAnimMenu(!animMenu);
+                    dispatch(toogle());
+
+                }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 200 200">
+                        <g strokeWidth="8.5" strokeLinecap="round">
+                            <path
+                                d="M72 82.286h28.75"
+                                fill="#009100"
+                                fillRule="evenodd"
+                                stroke="#fff"
+                            />
+                            <path
+                                d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
+                                fill="none"
+                                stroke="#fff"
+                            />
+                            <path
+                                d="M72 125.143h28.75"
+                                fill="#009100"
+                                fillRule="evenodd"
+                                stroke="#fff"
+                            />
+                            <path
+                                d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
+                                fill="none"
+                                stroke="#fff"
+                            />
+                            <path
+                                d="M100.75 82.286h28.75"
+                                fill="#009100"
+                                fillRule="evenodd"
+                                stroke="#fff"
+                            />
+                            <path
+                                d="M100.75 125.143h28.75"
+                                fill="#009100"
+                                fillRule="evenodd"
+                                stroke="#fff"
+                            />
+                        </g>
+                    </svg>
                 </div>
                 {/*<MoonIcon className={styles.moon}/>*/}
             </div>
